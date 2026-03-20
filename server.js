214c513f1fd3c8a408b925ef183daa8c92abe1e5
Pradeep-patel06 <pradeeppatel052006@gmail.com>
2026-03-20 18:33:58 +0530

    update project

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const Razorpay = require("razorpay");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+
+app.use(express.static(__dirname));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const razorpay = new Razorpay({
   key_id: "rzp_test_ST6tzo5xqfNtgJ",
@@ -12,7 +22,6 @@ const razorpay = new Razorpay({
 });
 
 app.post("/create-order", async (req, res) => {
-
   const options = {
     amount: req.body.amount,
     currency: "INR",
