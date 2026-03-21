c66a2b0f41eee0c8455638fa07cadd4845c39d0c
Pradeep-patel06 <pradeeppatel052006@gmail.com>
2026-03-21 13:03:02 +0530

    update project

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
