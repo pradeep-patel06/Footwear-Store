ecb496b2a6a63e552b6d342c4983cb04ca812d5b
Pradeep-patel06 <pradeeppatel052006@gmail.com>
2026-03-23 23:18:06 +0530

    Removed .env for security

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch(err => console.log(err));
 
